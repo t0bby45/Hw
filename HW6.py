2a88b8f9c0e5849f91b91495205f09b99ff69ccb
Kazan_bas <torehan195@gmail.com>
2024-11-10 08:59:21 +0500

    Add files via upload

---
+++ HW6.py
@@ -0,0 +1,20 @@
+result = []
+
+def divider(a, b):
+    if a < b:
+        raise ValueError(f"ValueError: {a} is less than {b}")
+    if b > 100:
+        raise IndexError(f"IndexError: {b} is greater than 100")
+    return a / b
+
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, (1, 2, 3): 15, 8: 4}
+
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except Exception as e:
+        print(f"Exception occurred for key {key}: {e}")
+
+print(result)
